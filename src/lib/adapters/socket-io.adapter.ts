48803705ade87dc0dec991b4aadca939e1d5b304
Ansat Jartybayev <lud1crous@mail.ru>
2021-03-29 17:27:18 +0600

    feat(app): socket.io-adapter added

---
+++ src/lib/adapters/socket-io.adapter.ts
@@ -0,0 +1,92 @@
+import {INestApplicationContext} from "@nestjs/common";
+import {AbstractWsAdapter, MessageMappingProperties} from "@nestjs/websockets";
+import {isFunction, isNil} from "@nestjs/common/utils/shared.utils";
+import {DISCONNECT_EVENT} from "@nestjs/websockets/constants";
+import {Server, ServerOptions} from "socket.io";
+import {fromEvent, Observable} from "rxjs";
+import {filter, first, map, mergeMap, share, takeUntil} from "rxjs/operators";
+
+export class SocketIoAdapter extends AbstractWsAdapter {
+  constructor(app: INestApplicationContext, private origin?: any) {
+    super(app);
+  }
+
+  public create(port: number, options?: any): Server {
+    if (!options) this.createIOServer(port);
+
+    const {namespace, server, ...opt} = options;
+
+    return server && isFunction(server.of)
+      ? server.of(namespace)
+      : namespace
+      ? this.createIOServer(port, opt).of(namespace)
+      : this.createIOServer(port, opt);
+  }
+
+  public createIOServer(
+    port: number,
+    options?: Partial<ServerOptions>
+  ): Server {
+    if (this.httpServer && port === 0) {
+      return new Server(this.httpServer, {
+        cors: {
+          origin: this.origin,
+          methods: ["GET", "POST"],
+          credentials: true
+        },
+        cookie: {
+          httpOnly: true,
+          path: "/"
+        },
+        maxHttpBufferSize: 1e6
+      });
+    }
+    return new Server(port, options);
+  }
+
+  public bindMessageHandlers(
+    client: any,
+    handlers: MessageMappingProperties[],
+    transform: (data: any) => Observable<any>
+  ) {
+    const disconnect$ = fromEvent(client, DISCONNECT_EVENT).pipe(
+      share(),
+      first()
+    );
+
+    handlers.forEach(({message, callback}) => {
+      const source$ = fromEvent(client, message).pipe(
+        mergeMap((payload: any) => {
+          const {data, ack} = this.mapPayload(payload);
+          return transform(callback(data, ack)).pipe(
+            filter((response: any) => !isNil(response)),
+            map((response: any) => [response, ack])
+          );
+        }),
+        takeUntil(disconnect$)
+      );
+      source$.subscribe(([response, ack]) => {
+        if (response.event) {
+          return client.emit(response.event, response.data);
+        }
+        isFunction(ack) && ack(response);
+      });
+    });
+  }
+
+  public mapPayload(payload: any): {data: any; ack?: () => any} {
+    if (!Array.isArray(payload)) {
+      return {data: payload};
+    }
+    const lastElement = payload[payload.length - 1];
+    const isAck = isFunction(lastElement);
+    if (isAck) {
+      const size = payload.length - 1;
+      return {
+        data: size === 1 ? payload[0] : payload.slice(0, size),
+        ack: lastElement
+      };
+    }
+    return {data: payload};
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {ValidationPipe} from "@nestjs/common";
 import {NestFactory} from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 
+import {SocketIoAdapter} from "@lib/adapters";
 import {AppModule} from "./app.module";
 
 async function bootstrap() {
@@ -11,6 +12,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({transform: true}));
 
   app.use(cookieParser());
+
+  app.useWebSocketAdapter(new SocketIoAdapter(app));
 
   await app.listen(process.env.PORT);
 }
