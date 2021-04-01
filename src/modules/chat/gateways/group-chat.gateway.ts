d273c38af02a6c1e5bb739b0dd1b04fd3a1b2a96
Ansat Jartybayev <lud1crous@mail.ru>
2021-04-01 22:03:15 +0600

    feat(modules/chat): added group-chat gateway

--- src/modules/chat/gateways/group-chat.gateway.ts
+++ src/modules/chat/gateways/group-chat.gateway.ts
@@ -2,10 +2,132 @@ import {
   ConnectedSocket,
   SubscribeMessage,
   WebSocketGateway,
-  OnGatewayConnection
+  WebSocketServer,
+  MessageBody
 } from "@nestjs/websockets";
+import {BadRequestException} from "@nestjs/common";
+import {Server} from "socket.io";
 
-import {ExtendedSocket} from "@lib/typings";
+import {ExtendedSocket, ID} from "@lib/typings";
+import {WebsocketsService} from "@lib/websockets";
+import {GroupChatMemberService} from "../services";
+import {ChatMessagePublicData} from "../lib/typings";
+
+interface JoinEventBody {
+  chatId: ID;
+}
+
+interface CreatingChatEventBody {
+  chatId: ID;
+  users: ID[];
+}
+
+interface MessageSendingEventBody {
+  message: ChatMessagePublicData;
+}
+
+interface MessageReadingEventBody {
+  messages: ID[];
+  chatId: ID;
+}
+
+const prefix = "GROUP_CHAT";
+
+const events = {
+  JOIN: `${prefix}:JOIN`,
+  CREATING_CHAT: `${prefix}:CREATING_CHAT`,
+  MESSAGE_READING: `${prefix}:MESSAGE_READING`,
+  MESSAGE_SENDING: `${prefix}:MESSAGE_SENDING`
+};
+
+const clientEvents = {
+  MESSAGE_READING: `${prefix}:MESSAGE_READING`,
+  MESSAGE_SENDING: `${prefix}:MESSAGE_SENDING`
+};
+
+const error = new BadRequestException("Invalid credentials.");
 
 @WebSocketGateway()
-export class GroupChatGateway {}
+export class GroupChatGateway {
+  constructor(
+    private readonly websocketsService: WebsocketsService,
+    private readonly memberService: GroupChatMemberService
+  ) {}
+
+  @WebSocketServer()
+  wss: Server;
+
+  @SubscribeMessage(events.CREATING_CHAT)
+  async handleCreatingChatEvent(
+    @ConnectedSocket() client: ExtendedSocket,
+    @MessageBody() {chatId, users}: CreatingChatEventBody
+  ): Promise<void> {
+    const hasAccess = !!(await this.memberService.findOne({
+      where: {user: {id: client.userId}, chat: {id: chatId}}
+    }));
+
+    if (!hasAccess) throw error;
+
+    const clients = this.websocketsService.getSocketsByUserId(client.userId);
+
+    clients.forEach(client => client.join(chatId));
+
+    for (let i = 0; i < users.length; i++) {
+      const id = users[i];
+
+      const hasAccess = !!(await this.memberService.findOne({
+        where: {user: {id}, chat: {id: chatId}}
+      }));
+
+      if (!hasAccess) return;
+
+      const sockets = this.websocketsService.getSocketsByUserId(id);
+
+      sockets.forEach(socket => socket.join(chatId));
+    }
+  }
+
+  @SubscribeMessage(events.JOIN)
+  async handleJoinEvent(
+    @ConnectedSocket() client: ExtendedSocket,
+    @MessageBody() {chatId}: JoinEventBody
+  ): Promise<void> {
+    const hasAccess = !!(await this.memberService.findOne({
+      where: {user: {id: client.userId}, chat: {id: chatId}}
+    }));
+
+    if (!hasAccess) throw error;
+
+    const clients = this.websocketsService.getSocketsByUserId(client.userId);
+
+    clients.forEach(client => client.join(chatId));
+  }
+
+  @SubscribeMessage(events.MESSAGE_SENDING)
+  async handleMessageSendingEvent(
+    @ConnectedSocket() client: ExtendedSocket,
+    @MessageBody() {message}: MessageSendingEventBody
+  ): Promise<void> {
+    const hasAccess = !!(await this.memberService.findOne({
+      where: {user: {id: client.userId}, chat: {id: message.chatId}}
+    }));
+
+    if (!hasAccess) throw error;
+
+    client.to(message.chatId).emit(clientEvents.MESSAGE_SENDING, {message});
+  }
+
+  @SubscribeMessage(events.MESSAGE_READING)
+  async handleMessageReadingEvent(
+    @ConnectedSocket() client: ExtendedSocket,
+    @MessageBody() {messages, chatId}: MessageReadingEventBody
+  ): Promise<void> {
+    const hasAccess = !!(await this.memberService.findOne({
+      where: {user: {id: client.userId}, chat: {id: chatId}}
+    }));
+
+    if (!hasAccess) throw error;
+
+    client.to(chatId).emit(clientEvents.MESSAGE_SENDING, {messages, chatId});
+  }
+}
